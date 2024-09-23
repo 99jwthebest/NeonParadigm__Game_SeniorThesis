bb0ea0f347079d13802c38e004765ae431bb3e21
Angieee3d <157183811+Angieee3d@users.noreply.github.com>
2024-09-22 19:08:21 -0500

    enviro blockin pass 1
    did a general setting blockin for all areas

--- NeonParadigm_Game/Intermediate/Build/BuildRulesProjects/NeonParadigm_GameModuleRules/obj/Development/NeonParadigm_GameModuleRules.AssemblyInfo.cs
+++ NeonParadigm_Game/Intermediate/Build/BuildRulesProjects/NeonParadigm_GameModuleRules/obj/Development/NeonParadigm_GameModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e832a2968abe212c42f77555621879f9a2a0233")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+644b71aa239068cdeee4cc902fcb7f2a5772238a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
