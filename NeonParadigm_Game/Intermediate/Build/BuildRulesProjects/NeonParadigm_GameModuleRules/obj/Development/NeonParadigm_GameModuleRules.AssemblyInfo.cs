067529b587557aa942cf5e302b9e34c341bd22a5
99jwthebest <johern12@student.uiwtx.edu>
2024-09-25 20:47:01 -0500

    Implemented_SaveRhythmAttack

--- NeonParadigm_Game/Intermediate/Build/BuildRulesProjects/NeonParadigm_GameModuleRules/obj/Development/NeonParadigm_GameModuleRules.AssemblyInfo.cs
+++ NeonParadigm_Game/Intermediate/Build/BuildRulesProjects/NeonParadigm_GameModuleRules/obj/Development/NeonParadigm_GameModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+457a60c7d78dfd3efbf53224767e4255900062d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6a711701ebbc6aad0252b998962249c03e98aa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
