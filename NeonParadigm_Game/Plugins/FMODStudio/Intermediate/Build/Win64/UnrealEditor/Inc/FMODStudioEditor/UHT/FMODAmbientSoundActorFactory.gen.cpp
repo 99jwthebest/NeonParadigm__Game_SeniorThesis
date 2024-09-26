f62f5f54f8c5889bbdbf0b05ed43d2d988d18364
Angieee3d <157183811+Angieee3d@users.noreply.github.com>
2024-09-26 13:07:52 -0500

    added a computer

--- NeonParadigm_Game/Plugins/FMODStudio/Intermediate/Build/Win64/UnrealEditor/Inc/FMODStudioEditor/UHT/FMODAmbientSoundActorFactory.gen.cpp
+++ NeonParadigm_Game/Plugins/FMODStudio/Intermediate/Build/Win64/UnrealEditor/Inc/FMODStudioEditor/UHT/FMODAmbientSoundActorFactory.gen.cpp
@@ -38,15 +38,11 @@ void EmptyLinkFunctionForGeneratedCodeFMODAmbientSoundActorFactory() {}
 	static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_UFMODAmbientSoundActorFactory_Statics::DependentSingletons) < 16);
 #if WITH_METADATA
 	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UFMODAmbientSoundActorFactory_Statics::Class_MetaDataParams[] = {
-#if !UE_BUILD_SHIPPING
 		{ "Comment", "/** FMOD Ambient Sound Actor Factory.\n*/" },
-#endif
 		{ "HideCategories", "Object Object" },
 		{ "IncludePath", "FMODAmbientSoundActorFactory.h" },
 		{ "ModuleRelativePath", "Classes/FMODAmbientSoundActorFactory.h" },
-#if !UE_BUILD_SHIPPING
 		{ "ToolTip", "FMOD Ambient Sound Actor Factory." },
-#endif
 	};
 #endif
 	const FCppClassTypeInfoStatic Z_Construct_UClass_UFMODAmbientSoundActorFactory_Statics::StaticCppClassTypeInfo = {
@@ -81,15 +77,15 @@ void EmptyLinkFunctionForGeneratedCodeFMODAmbientSoundActorFactory() {}
 	}
 	DEFINE_VTABLE_PTR_HELPER_CTOR(UFMODAmbientSoundActorFactory);
 	UFMODAmbientSoundActorFactory::~UFMODAmbientSoundActorFactory() {}
-	struct Z_CompiledInDeferFile_FID_Users_josep_Documents_Unreal_Projects_NeonParadigm__Game_SeniorThesis_NeonParadigm_Game_Plugins_FMODStudio_Source_FMODStudioEditor_Classes_FMODAmbientSoundActorFactory_h_Statics
+	struct Z_CompiledInDeferFile_FID_HostProject_Plugins_FMODStudio_Source_FMODStudioEditor_Classes_FMODAmbientSoundActorFactory_h_Statics
 	{
 		static const FClassRegisterCompiledInInfo ClassInfo[];
 	};
-	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_josep_Documents_Unreal_Projects_NeonParadigm__Game_SeniorThesis_NeonParadigm_Game_Plugins_FMODStudio_Source_FMODStudioEditor_Classes_FMODAmbientSoundActorFactory_h_Statics::ClassInfo[] = {
-		{ Z_Construct_UClass_UFMODAmbientSoundActorFactory, UFMODAmbientSoundActorFactory::StaticClass, TEXT("UFMODAmbientSoundActorFactory"), &Z_Registration_Info_UClass_UFMODAmbientSoundActorFactory, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(UFMODAmbientSoundActorFactory), 3289282180U) },
+	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_HostProject_Plugins_FMODStudio_Source_FMODStudioEditor_Classes_FMODAmbientSoundActorFactory_h_Statics::ClassInfo[] = {
+		{ Z_Construct_UClass_UFMODAmbientSoundActorFactory, UFMODAmbientSoundActorFactory::StaticClass, TEXT("UFMODAmbientSoundActorFactory"), &Z_Registration_Info_UClass_UFMODAmbientSoundActorFactory, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(UFMODAmbientSoundActorFactory), 979798992U) },
 	};
-	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_josep_Documents_Unreal_Projects_NeonParadigm__Game_SeniorThesis_NeonParadigm_Game_Plugins_FMODStudio_Source_FMODStudioEditor_Classes_FMODAmbientSoundActorFactory_h_745873146(TEXT("/Script/FMODStudioEditor"),
-		Z_CompiledInDeferFile_FID_Users_josep_Documents_Unreal_Projects_NeonParadigm__Game_SeniorThesis_NeonParadigm_Game_Plugins_FMODStudio_Source_FMODStudioEditor_Classes_FMODAmbientSoundActorFactory_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_josep_Documents_Unreal_Projects_NeonParadigm__Game_SeniorThesis_NeonParadigm_Game_Plugins_FMODStudio_Source_FMODStudioEditor_Classes_FMODAmbientSoundActorFactory_h_Statics::ClassInfo),
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_HostProject_Plugins_FMODStudio_Source_FMODStudioEditor_Classes_FMODAmbientSoundActorFactory_h_3717781988(TEXT("/Script/FMODStudioEditor"),
+		Z_CompiledInDeferFile_FID_HostProject_Plugins_FMODStudio_Source_FMODStudioEditor_Classes_FMODAmbientSoundActorFactory_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_HostProject_Plugins_FMODStudio_Source_FMODStudioEditor_Classes_FMODAmbientSoundActorFactory_h_Statics::ClassInfo),
 		nullptr, 0,
 		nullptr, 0);
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
