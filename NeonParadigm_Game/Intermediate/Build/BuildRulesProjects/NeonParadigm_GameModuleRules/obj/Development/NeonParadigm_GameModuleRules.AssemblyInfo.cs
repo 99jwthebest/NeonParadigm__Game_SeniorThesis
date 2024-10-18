dfc2c430e802d376b7914943d67d55f9d4e35025
99jwthebest <johern12@student.uiwtx.edu>
2024-10-17 21:27:48 -0500

    AddedModels

--- NeonParadigm_Game/Intermediate/Build/BuildRulesProjects/NeonParadigm_GameModuleRules/obj/Development/NeonParadigm_GameModuleRules.AssemblyInfo.cs
+++ NeonParadigm_Game/Intermediate/Build/BuildRulesProjects/NeonParadigm_GameModuleRules/obj/Development/NeonParadigm_GameModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3507cb7211c7a6bc86f072b73ff808eb0822d7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d71882860f3d5c9e599c564531efa5b08b58578")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
