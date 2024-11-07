0f8d48ba769acd656e4671e86fb7da44fb99a76f
99jwthebest <johern12@student.uiwtx.edu>
2024-11-07 12:28:19 -0600

    FinishedMainMenu

--- NeonParadigm_Game/Intermediate/Build/BuildRulesProjects/NeonParadigm_GameModuleRules/obj/Development/NeonParadigm_GameModuleRules.AssemblyInfo.cs
+++ NeonParadigm_Game/Intermediate/Build/BuildRulesProjects/NeonParadigm_GameModuleRules/obj/Development/NeonParadigm_GameModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb0cb98a833a4c89028b15851810aad0c26911f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebda83e26140c9c40c539ec6a424eb0d901c0d21")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
