753ffa67f16a8b11ff22a9ad59c00d22e6519792
AnimeWeeb02 <alesa7802@gmail.com>
2024-11-20 08:08:50 -0600

    Merge Sophia's Props from conflict
    Merged, pushed, and final push

--- NeonParadigm_Game/Intermediate/Build/BuildRulesProjects/NeonParadigm_GameModuleRules/obj/Development/NeonParadigm_GameModuleRules.AssemblyInfo.cs
+++ NeonParadigm_Game/Intermediate/Build/BuildRulesProjects/NeonParadigm_GameModuleRules/obj/Development/NeonParadigm_GameModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bebcf450a293978b542f8abdfce61696d28da6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ef3a595e03b8b096ca360251cc87ebfede651f")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- NeonParadigm_Game/Intermediate/Build/Win64/x64/NeonParadigm_GameEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.cpp.old
+++ NeonParadigm_Game/Intermediate/Build/Win64/x64/NeonParadigm_GameEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.cpp.old
@@ -1,2 +1,2 @@
-// Compiler: 14.41.34120
+// Compiler: 14.40.33812
 #include "E:/senior thesis/NEON/github/NeonParadigm__Game_SeniorThesis/NeonParadigm_Game/Intermediate/Build/Win64/x64/NeonParadigm_GameEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h"
