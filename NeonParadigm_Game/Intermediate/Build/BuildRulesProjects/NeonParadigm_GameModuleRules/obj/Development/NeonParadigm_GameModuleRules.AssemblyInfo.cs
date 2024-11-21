64c9b49aa073b8921926f5ef941c049dc50bcbbb
Angieee3d <157183811+Angieee3d@users.noreply.github.com>
2024-11-21 11:53:29 -0600

    changes

--- NeonParadigm_Game/Intermediate/Build/BuildRulesProjects/NeonParadigm_GameModuleRules/obj/Development/NeonParadigm_GameModuleRules.AssemblyInfo.cs
+++ NeonParadigm_Game/Intermediate/Build/BuildRulesProjects/NeonParadigm_GameModuleRules/obj/Development/NeonParadigm_GameModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61785299d21abd8f113c053d119f08323e135f83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcac607b882d93190bb4add50aa6139370de81ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeonParadigm_GameModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
