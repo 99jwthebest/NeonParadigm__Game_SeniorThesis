e9fcc956119cb317cd6817cc2980a5996a7945c8
99jwthebest <johern12@student.uiwtx.edu>
2024-12-03 14:45:20 -0600

    cleaned project

--- NeonParadigm_Game/Intermediate/Build/Win64/x64/NeonParadigm_GameEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/Users/josep/Documents/Unreal Projects/NeonParadigm__Game_SeniorThesis/NeonParadigm_Game/Intermediate/Build/Win64/x64/NeonParadigm_GameEditor/Development/Core/SharedDefinitions.Core.RTTI.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
