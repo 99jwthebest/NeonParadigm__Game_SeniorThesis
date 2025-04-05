d74e4ab5101b222f9b8046b278f68d0a0aa2f6ef
99jwthebest <johern12@student.uiwtx.edu>
2025-04-05 18:33:02 -0500

    GettingsThingsDone

--- NeonParadigm_Game/Plugins/FMODStudio/Intermediate/Build/Win64/UnrealEditor/Inc/FMODStudio/UHT/FMODStudio.init.gen.cpp
+++ NeonParadigm_Game/Plugins/FMODStudio/Intermediate/Build/Win64/UnrealEditor/Inc/FMODStudio/UHT/FMODStudio.init.gen.cpp
@@ -27,7 +27,7 @@ void EmptyLinkFunctionForGeneratedCodeFMODStudio_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0xEDB827B0,
+				0x3A78095B,
 				0x4092B8F0,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -35,5 +35,5 @@ void EmptyLinkFunctionForGeneratedCodeFMODStudio_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_FMODStudio.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_FMODStudio(Z_Construct_UPackage__Script_FMODStudio, TEXT("/Script/FMODStudio"), Z_Registration_Info_UPackage__Script_FMODStudio, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xEDB827B0, 0x4092B8F0));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_FMODStudio(Z_Construct_UPackage__Script_FMODStudio, TEXT("/Script/FMODStudio"), Z_Registration_Info_UPackage__Script_FMODStudio, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x3A78095B, 0x4092B8F0));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
